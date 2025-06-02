7303ef88360e288837fdf5d6da44fc660dcb2400
alisto-stack <alisto55555@gmail.com>
2025-06-02 22:25:11 +0100

    Delete find_string.sh

--- find_string.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read cafe
-read chaine
-if grep -q "$chaine" "$cafe"; then
-echo "La chaîne '$chaine' a été trouvée dans $cafe."
-else
-echo "La chaîne '$chaine' n'a pas été trouvée dans $cafe."
-fi
