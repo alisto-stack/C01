90357d64329b3606411d954f09a51e1f21d56829
alisto-stack <alisto55555@gmail.com>
2025-06-02 22:32:09 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+#nous allons creer deux variables
+read f
+#celle la contient le fichier 
+read c
+#et cellle la contient la chaine 
+#nous allons maintenant proceder a la recherche de la chaine dans le fichier a l'aide de grep
+if grep -q "$c" "$f"; then 
+echo "La chaîne '$c' a été trouvée dans $f."
+else
+echo "La chaîne '$c' n'a pas été trouvée dans $f."
+fi
+exit 0 
